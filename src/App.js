189927d675dd37ddf8fcdcbcefcaa166f396ea9d
PredragBecukic <p.becukic2@gmail.com>
2025-04-18 20:08:38 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'Arial' }}>
+      <h1>AI Inbox Asistent - Version 5</h1>
+      <p>Ova verzija je spremna za povezivanje sa Facebook/Instagram porukama.</p>
+    </div>
+  );
+}
+
+export default App;
